5171e9b28f5c5bc040a1884d728a0835d15fe242
Jay <jaychakra.jc@gmail.com>
2025-11-05 01:24:00 +0530

    Till async programming

--- S2-RestAPIs/5-ImplementPostAPIs/app.js
+++ S2-RestAPIs/5-ImplementPostAPIs/app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
@@ -80,6 +81,8 @@ app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
-app.listen(3000, () => {
-  console.log('Server is running on port 3000');
+const PORT = process.env.PORT
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
